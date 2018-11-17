9cef6544d84e0f6c859cf69e89440f8b93eed259
Sam Goldstein <sam@stackery.io>
2018-11-16 23:35:00 -0800

    Updated trigger function

--- src/function8791253A/index.js
+++ src/function8791253A/index.js
@@ -1,5 +1,11 @@
 exports.handler = async message => {
   console.log(message);
-
-  return {};
+  var ecs = new AWS.ECS({apiVersion: '2014-11-13'});
+   var params = {
+       desiredCount: 1,
+       serviceName: "ward-neo4j",
+       taskDefinition: process.env.DOCKER_TASK_ARN
+      };
+  console.log(params);
+  return ecs.createService(params).promise();
 }
