d2a02cb615760a31376241f716cb4345e0302bef
Sam Goldstein <sam@stackery.io>
2018-11-17 15:24:20 -0800

    Fix syntax

--- src/stopNeo4j/index.js
+++ src/stopNeo4j/index.js
@@ -5,10 +5,9 @@ exports.handler = async message => {
   const ecs = new AWS.ECS({apiVersion: '2014-11-13'});
   console.log(process.env)
   const params = {
-      task: process.env.DOCKER_TASK_ARN,
-      cluster: 'default',
-    };
+    task: process.env.DOCKER_TASK_ARN,
+    cluster: 'default',
   };
   console.log(params);
   return ecs.stopTask(params).promise();
-}
+};
