4ff41958c8408e83918ff5fb8867e995774a20e5
Sam Goldstein <sam@stackery.io>
2018-11-16 23:45:32 -0800

    Fix fargate params

--- src/function8791253A/index.js
+++ src/function8791253A/index.js
@@ -2,9 +2,9 @@ const AWS = require('aws-sdk')
 exports.handler = async message => {
   console.log(message);
   const ecs = new AWS.ECS({apiVersion: '2014-11-13'});
+
   const params = {
-    desiredCount: 1,
-    serviceName: 'ward-neo4j',
+    cluster: 'default',
     taskDefinition: process.env.DOCKER_TASK_ARN
   };
   console.log(params);
