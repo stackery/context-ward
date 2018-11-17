d25ad090a0bebaf8ef59629bdb84ad5e103fe325
Sam Goldstein <sam@stackery.io>
2018-11-16 23:38:05 -0800

    Require aws

--- src/function8791253A/index.js
+++ src/function8791253A/index.js
@@ -1,11 +1,12 @@
+const AWS = require('aws-sdk')
 exports.handler = async message => {
   console.log(message);
-  var ecs = new AWS.ECS({apiVersion: '2014-11-13'});
-   var params = {
-       desiredCount: 1,
-       serviceName: "ward-neo4j",
-       taskDefinition: process.env.DOCKER_TASK_ARN
-      };
+  const ecs = new AWS.ECS({apiVersion: '2014-11-13'});
+  const params = {
+    desiredCount: 1,
+    serviceName: 'ward-neo4j',
+    taskDefinition: process.env.DOCKER_TASK_ARN
+  };
   console.log(params);
   return ecs.createService(params).promise();
 }
