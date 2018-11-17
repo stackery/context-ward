ab530b56df861d46535e54897ab8885fe9f15b3d
Sam Goldstein <sam@stackery.io>
2018-11-16 23:59:12 -0800

    Add subnets

--- src/function8791253A/index.js
+++ src/function8791253A/index.js
@@ -2,9 +2,9 @@ const AWS = require('aws-sdk')
 exports.handler = async message => {
   console.log(message);
   const ecs = new AWS.ECS({apiVersion: '2014-11-13'});
-
   const params = {
     cluster: 'default',
+    subnets: JSON.parse(process.env.DOCKER_TASK_SUBNETS),
     taskDefinition: process.env.DOCKER_TASK_ARN
   };
   console.log(params);
