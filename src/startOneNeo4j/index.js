a2c14b7e183db670327fff756231090b611b60d1
Sam Goldstein <sam@stackery.io>
2018-11-18 21:38:28 -0800

    Add startOneNeo code

--- src/startOneNeo4j/index.js
+++ src/startOneNeo4j/index.js
@@ -1,5 +1,36 @@
+const AWS = require('aws-sdk')
+
 exports.handler = async message => {
   console.log(message);
+  const ecs = new AWS.ECS({apiVersion: '2014-11-13'});
+  console.log(process.env)
+  try {
+    const listResponse = await ecs.listTasks().promise();
+    console.log(listResponse)
+    const promises = []
+    if (listResponse.taskArns[0]){
+      return {
+        statusCode: 200,
+        body: `instance already running`
+      }
 
-  return {};
-}
+    } else {
+
+      const params = {
+        FunctionName: process.env.FUNCTION_NAME,
+        InvokeArgs: '{}' 
+       };
+       await lambda.invokeAsync(params).promise();
+      return {
+        statusCode: 200,
+        body: `started neo4j`
+      }
+    }
+      
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e.message
+    }
+  }
+};
