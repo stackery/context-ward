c20ae588806bf85f45f18cc23a7000ac3467ca55
Sam Goldstein <sam@stackery.io>
2018-11-18 22:24:11 -0800

    Status endpoint

--- src/status/index.js
+++ src/status/index.js
@@ -1,5 +1,20 @@
+const AWS = require('aws-sdk');
+
 exports.handler = async message => {
   console.log(message);
-
-  return {};
-}
+  const ecs = new AWS.ECS({ apiVersion: '2014-11-13' });
+  console.log(process.env);
+  try {
+    const listResponse = await ecs.listTasks().promise();
+    console.log(listResponse);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(listResponse.taskArns)
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e.message
+    };
+  }
+};
--- src/stopNeo4j/index.js
+++ src/stopNeo4j/index.js
@@ -1,15 +1,15 @@
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 
 exports.handler = async message => {
   console.log(message);
-  const ecs = new AWS.ECS({apiVersion: '2014-11-13'});
-  console.log(process.env)
+  const ecs = new AWS.ECS({ apiVersion: '2014-11-13' });
+  console.log(process.env);
   try {
     const listResponse = await ecs.listTasks().promise();
-    console.log(listResponse)
-    const promises = []
+    console.log(listResponse);
+    const promises = [];
     listResponse.taskArns && listResponse.taskArns.forEach(arn => {
-      const stopPromise = ecs.stopTask({task: arn}).promise();
+      const stopPromise = ecs.stopTask({ task: arn }).promise();
       promises.push(stopPromise);
     });
     const results = await Promise.all(promises);
@@ -17,11 +17,11 @@ exports.handler = async message => {
     return {
       statusCode: 200,
       body: `neo4j stopped`
-    }
+    };
   } catch (e) {
     return {
       statusCode: 500,
       body: e.message
-    }
+    };
   }
 };
