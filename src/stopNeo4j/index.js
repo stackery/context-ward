b8506c591d5175502cde4daa7416198a24a37f1b
Sam Goldstein <sam@stackery.io>
2018-11-18 16:12:38 -0800

    Remove dead code

--- src/stopNeo4j/index.js
+++ src/stopNeo4j/index.js
@@ -4,10 +4,6 @@ exports.handler = async message => {
   console.log(message);
   const ecs = new AWS.ECS({apiVersion: '2014-11-13'});
   console.log(process.env)
-  const params = {
-    task: process.env.DOCKER_TASK_ARN,
-  };
-  console.log(params);
   try {
     const listResponse = await ecs.listTasks().promise();
     console.log(listResponse)
